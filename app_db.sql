61529fb20022e57e1df3161e72a0c72ac6de5573
Faiz Akram <faiz.krm@gmail.com>
2018-07-16 20:04:46 +0530

    DB Commited

--- app_db.sql
+++ app_db.sql
@@ -12,9 +12,240 @@ MySQL - 10.0.34-MariaDB-0ubuntu0.16.04.1 : Database - app_db
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`app_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`app_db` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
 USE `app_db`;
+
+/*Table structure for table `QRTZ_BLOB_TRIGGERS` */
+
+DROP TABLE IF EXISTS `QRTZ_BLOB_TRIGGERS`;
+
+CREATE TABLE `QRTZ_BLOB_TRIGGERS` (
+  `SCHED_NAME` varchar(120) NOT NULL,
+  `TRIGGER_NAME` varchar(200) NOT NULL,
+  `TRIGGER_GROUP` varchar(200) NOT NULL,
+  `BLOB_DATA` blob,
+  PRIMARY KEY (`SCHED_NAME`,`TRIGGER_NAME`,`TRIGGER_GROUP`),
+  KEY `SCHED_NAME` (`SCHED_NAME`,`TRIGGER_NAME`,`TRIGGER_GROUP`),
+  CONSTRAINT `QRTZ_BLOB_TRIGGERS_ibfk_1` FOREIGN KEY (`SCHED_NAME`, `TRIGGER_NAME`, `TRIGGER_GROUP`) REFERENCES `QRTZ_TRIGGERS` (`SCHED_NAME`, `TRIGGER_NAME`, `TRIGGER_GROUP`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `QRTZ_BLOB_TRIGGERS` */
+
+/*Table structure for table `QRTZ_CALENDARS` */
+
+DROP TABLE IF EXISTS `QRTZ_CALENDARS`;
+
+CREATE TABLE `QRTZ_CALENDARS` (
+  `SCHED_NAME` varchar(120) NOT NULL,
+  `CALENDAR_NAME` varchar(200) NOT NULL,
+  `CALENDAR` blob NOT NULL,
+  PRIMARY KEY (`SCHED_NAME`,`CALENDAR_NAME`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `QRTZ_CALENDARS` */
+
+/*Table structure for table `QRTZ_CRON_TRIGGERS` */
+
+DROP TABLE IF EXISTS `QRTZ_CRON_TRIGGERS`;
+
+CREATE TABLE `QRTZ_CRON_TRIGGERS` (
+  `SCHED_NAME` varchar(120) NOT NULL,
+  `TRIGGER_NAME` varchar(200) NOT NULL,
+  `TRIGGER_GROUP` varchar(200) NOT NULL,
+  `CRON_EXPRESSION` varchar(120) NOT NULL,
+  `TIME_ZONE_ID` varchar(80) DEFAULT NULL,
+  PRIMARY KEY (`SCHED_NAME`,`TRIGGER_NAME`,`TRIGGER_GROUP`),
+  CONSTRAINT `QRTZ_CRON_TRIGGERS_ibfk_1` FOREIGN KEY (`SCHED_NAME`, `TRIGGER_NAME`, `TRIGGER_GROUP`) REFERENCES `QRTZ_TRIGGERS` (`SCHED_NAME`, `TRIGGER_NAME`, `TRIGGER_GROUP`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `QRTZ_CRON_TRIGGERS` */
+
+insert  into `QRTZ_CRON_TRIGGERS`(`SCHED_NAME`,`TRIGGER_NAME`,`TRIGGER_GROUP`,`CRON_EXPRESSION`,`TIME_ZONE_ID`) values 
+('quartzScheduler','processMyJobTrigger','DEFAULT','0 0/1 * * * ?','Asia/Calcutta');
+
+/*Table structure for table `QRTZ_FIRED_TRIGGERS` */
+
+DROP TABLE IF EXISTS `QRTZ_FIRED_TRIGGERS`;
+
+CREATE TABLE `QRTZ_FIRED_TRIGGERS` (
+  `SCHED_NAME` varchar(120) NOT NULL,
+  `ENTRY_ID` varchar(95) NOT NULL,
+  `TRIGGER_NAME` varchar(200) NOT NULL,
+  `TRIGGER_GROUP` varchar(200) NOT NULL,
+  `INSTANCE_NAME` varchar(200) NOT NULL,
+  `FIRED_TIME` bigint(13) NOT NULL,
+  `SCHED_TIME` bigint(13) NOT NULL,
+  `PRIORITY` int(11) NOT NULL,
+  `STATE` varchar(16) NOT NULL,
+  `JOB_NAME` varchar(200) DEFAULT NULL,
+  `JOB_GROUP` varchar(200) DEFAULT NULL,
+  `IS_NONCONCURRENT` varchar(1) DEFAULT NULL,
+  `REQUESTS_RECOVERY` varchar(1) DEFAULT NULL,
+  PRIMARY KEY (`SCHED_NAME`,`ENTRY_ID`),
+  KEY `IDX_QRTZ_FT_TRIG_INST_NAME` (`SCHED_NAME`,`INSTANCE_NAME`),
+  KEY `IDX_QRTZ_FT_INST_JOB_REQ_RCVRY` (`SCHED_NAME`,`INSTANCE_NAME`,`REQUESTS_RECOVERY`),
+  KEY `IDX_QRTZ_FT_J_G` (`SCHED_NAME`,`JOB_NAME`,`JOB_GROUP`),
+  KEY `IDX_QRTZ_FT_JG` (`SCHED_NAME`,`JOB_GROUP`),
+  KEY `IDX_QRTZ_FT_T_G` (`SCHED_NAME`,`TRIGGER_NAME`,`TRIGGER_GROUP`),
+  KEY `IDX_QRTZ_FT_TG` (`SCHED_NAME`,`TRIGGER_GROUP`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `QRTZ_FIRED_TRIGGERS` */
+
+/*Table structure for table `QRTZ_JOB_DETAILS` */
+
+DROP TABLE IF EXISTS `QRTZ_JOB_DETAILS`;
+
+CREATE TABLE `QRTZ_JOB_DETAILS` (
+  `SCHED_NAME` varchar(120) NOT NULL,
+  `JOB_NAME` varchar(200) NOT NULL,
+  `JOB_GROUP` varchar(200) NOT NULL,
+  `DESCRIPTION` varchar(250) DEFAULT NULL,
+  `JOB_CLASS_NAME` varchar(250) NOT NULL,
+  `IS_DURABLE` varchar(1) NOT NULL,
+  `IS_NONCONCURRENT` varchar(1) NOT NULL,
+  `IS_UPDATE_DATA` varchar(1) NOT NULL,
+  `REQUESTS_RECOVERY` varchar(1) NOT NULL,
+  `JOB_DATA` blob,
+  PRIMARY KEY (`SCHED_NAME`,`JOB_NAME`,`JOB_GROUP`),
+  KEY `IDX_QRTZ_J_REQ_RECOVERY` (`SCHED_NAME`,`REQUESTS_RECOVERY`),
+  KEY `IDX_QRTZ_J_GRP` (`SCHED_NAME`,`JOB_GROUP`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `QRTZ_JOB_DETAILS` */
+
+insert  into `QRTZ_JOB_DETAILS`(`SCHED_NAME`,`JOB_NAME`,`JOB_GROUP`,`DESCRIPTION`,`JOB_CLASS_NAME`,`IS_DURABLE`,`IS_NONCONCURRENT`,`IS_UPDATE_DATA`,`REQUESTS_RECOVERY`,`JOB_DATA`) values 
+('quartzScheduler','processMyJob','DEFAULT',NULL,'com.app.quartz.service.MyJob','1','1','0','0','¨Ì\0sr\0org.quartz.JobDataMapü∞ÉËø©∞À\0\0xr\0&org.quartz.utils.StringKeyDirtyFlagMapÇË√˚≈](\0Z\0allowsTransientDataxr\0org.quartz.utils.DirtyFlagMapÊ.≠(v\nŒ\0Z\0dirtyL\0mapt\0Ljava/util/Map;xp\0sr\0java.util.HashMap⁄¡√`—\0F\0\nloadFactorI\0	thresholdxp?@\0\0\0\0\0w\0\0\0\0\0\0\0x\0');
+
+/*Table structure for table `QRTZ_LOCKS` */
+
+DROP TABLE IF EXISTS `QRTZ_LOCKS`;
+
+CREATE TABLE `QRTZ_LOCKS` (
+  `SCHED_NAME` varchar(120) NOT NULL,
+  `LOCK_NAME` varchar(40) NOT NULL,
+  PRIMARY KEY (`SCHED_NAME`,`LOCK_NAME`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `QRTZ_LOCKS` */
+
+insert  into `QRTZ_LOCKS`(`SCHED_NAME`,`LOCK_NAME`) values 
+('quartzScheduler','STATE_ACCESS'),
+('quartzScheduler','TRIGGER_ACCESS');
+
+/*Table structure for table `QRTZ_PAUSED_TRIGGER_GRPS` */
+
+DROP TABLE IF EXISTS `QRTZ_PAUSED_TRIGGER_GRPS`;
+
+CREATE TABLE `QRTZ_PAUSED_TRIGGER_GRPS` (
+  `SCHED_NAME` varchar(120) NOT NULL,
+  `TRIGGER_GROUP` varchar(200) NOT NULL,
+  PRIMARY KEY (`SCHED_NAME`,`TRIGGER_GROUP`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `QRTZ_PAUSED_TRIGGER_GRPS` */
+
+/*Table structure for table `QRTZ_SCHEDULER_STATE` */
+
+DROP TABLE IF EXISTS `QRTZ_SCHEDULER_STATE`;
+
+CREATE TABLE `QRTZ_SCHEDULER_STATE` (
+  `SCHED_NAME` varchar(120) NOT NULL,
+  `INSTANCE_NAME` varchar(200) NOT NULL,
+  `LAST_CHECKIN_TIME` bigint(13) NOT NULL,
+  `CHECKIN_INTERVAL` bigint(13) NOT NULL,
+  PRIMARY KEY (`SCHED_NAME`,`INSTANCE_NAME`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `QRTZ_SCHEDULER_STATE` */
+
+insert  into `QRTZ_SCHEDULER_STATE`(`SCHED_NAME`,`INSTANCE_NAME`,`LAST_CHECKIN_TIME`,`CHECKIN_INTERVAL`) values 
+('quartzScheduler','COMPND-LTP-3891531750782611',1531751226973,20000);
+
+/*Table structure for table `QRTZ_SIMPLE_TRIGGERS` */
+
+DROP TABLE IF EXISTS `QRTZ_SIMPLE_TRIGGERS`;
+
+CREATE TABLE `QRTZ_SIMPLE_TRIGGERS` (
+  `SCHED_NAME` varchar(120) NOT NULL,
+  `TRIGGER_NAME` varchar(200) NOT NULL,
+  `TRIGGER_GROUP` varchar(200) NOT NULL,
+  `REPEAT_COUNT` bigint(7) NOT NULL,
+  `REPEAT_INTERVAL` bigint(12) NOT NULL,
+  `TIMES_TRIGGERED` bigint(10) NOT NULL,
+  PRIMARY KEY (`SCHED_NAME`,`TRIGGER_NAME`,`TRIGGER_GROUP`),
+  CONSTRAINT `QRTZ_SIMPLE_TRIGGERS_ibfk_1` FOREIGN KEY (`SCHED_NAME`, `TRIGGER_NAME`, `TRIGGER_GROUP`) REFERENCES `QRTZ_TRIGGERS` (`SCHED_NAME`, `TRIGGER_NAME`, `TRIGGER_GROUP`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `QRTZ_SIMPLE_TRIGGERS` */
+
+/*Table structure for table `QRTZ_SIMPROP_TRIGGERS` */
+
+DROP TABLE IF EXISTS `QRTZ_SIMPROP_TRIGGERS`;
+
+CREATE TABLE `QRTZ_SIMPROP_TRIGGERS` (
+  `SCHED_NAME` varchar(120) NOT NULL,
+  `TRIGGER_NAME` varchar(200) NOT NULL,
+  `TRIGGER_GROUP` varchar(200) NOT NULL,
+  `STR_PROP_1` varchar(512) DEFAULT NULL,
+  `STR_PROP_2` varchar(512) DEFAULT NULL,
+  `STR_PROP_3` varchar(512) DEFAULT NULL,
+  `INT_PROP_1` int(11) DEFAULT NULL,
+  `INT_PROP_2` int(11) DEFAULT NULL,
+  `LONG_PROP_1` bigint(20) DEFAULT NULL,
+  `LONG_PROP_2` bigint(20) DEFAULT NULL,
+  `DEC_PROP_1` decimal(13,4) DEFAULT NULL,
+  `DEC_PROP_2` decimal(13,4) DEFAULT NULL,
+  `BOOL_PROP_1` varchar(1) DEFAULT NULL,
+  `BOOL_PROP_2` varchar(1) DEFAULT NULL,
+  PRIMARY KEY (`SCHED_NAME`,`TRIGGER_NAME`,`TRIGGER_GROUP`),
+  CONSTRAINT `QRTZ_SIMPROP_TRIGGERS_ibfk_1` FOREIGN KEY (`SCHED_NAME`, `TRIGGER_NAME`, `TRIGGER_GROUP`) REFERENCES `QRTZ_TRIGGERS` (`SCHED_NAME`, `TRIGGER_NAME`, `TRIGGER_GROUP`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `QRTZ_SIMPROP_TRIGGERS` */
+
+/*Table structure for table `QRTZ_TRIGGERS` */
+
+DROP TABLE IF EXISTS `QRTZ_TRIGGERS`;
+
+CREATE TABLE `QRTZ_TRIGGERS` (
+  `SCHED_NAME` varchar(120) NOT NULL,
+  `TRIGGER_NAME` varchar(200) NOT NULL,
+  `TRIGGER_GROUP` varchar(200) NOT NULL,
+  `JOB_NAME` varchar(200) NOT NULL,
+  `JOB_GROUP` varchar(200) NOT NULL,
+  `DESCRIPTION` varchar(250) DEFAULT NULL,
+  `NEXT_FIRE_TIME` bigint(13) DEFAULT NULL,
+  `PREV_FIRE_TIME` bigint(13) DEFAULT NULL,
+  `PRIORITY` int(11) DEFAULT NULL,
+  `TRIGGER_STATE` varchar(16) NOT NULL,
+  `TRIGGER_TYPE` varchar(8) NOT NULL,
+  `START_TIME` bigint(13) NOT NULL,
+  `END_TIME` bigint(13) DEFAULT NULL,
+  `CALENDAR_NAME` varchar(200) DEFAULT NULL,
+  `MISFIRE_INSTR` smallint(2) DEFAULT NULL,
+  `JOB_DATA` blob,
+  PRIMARY KEY (`SCHED_NAME`,`TRIGGER_NAME`,`TRIGGER_GROUP`),
+  KEY `IDX_QRTZ_T_J` (`SCHED_NAME`,`JOB_NAME`,`JOB_GROUP`),
+  KEY `IDX_QRTZ_T_JG` (`SCHED_NAME`,`JOB_GROUP`),
+  KEY `IDX_QRTZ_T_C` (`SCHED_NAME`,`CALENDAR_NAME`),
+  KEY `IDX_QRTZ_T_G` (`SCHED_NAME`,`TRIGGER_GROUP`),
+  KEY `IDX_QRTZ_T_STATE` (`SCHED_NAME`,`TRIGGER_STATE`),
+  KEY `IDX_QRTZ_T_N_STATE` (`SCHED_NAME`,`TRIGGER_NAME`,`TRIGGER_GROUP`,`TRIGGER_STATE`),
+  KEY `IDX_QRTZ_T_N_G_STATE` (`SCHED_NAME`,`TRIGGER_GROUP`,`TRIGGER_STATE`),
+  KEY `IDX_QRTZ_T_NEXT_FIRE_TIME` (`SCHED_NAME`,`NEXT_FIRE_TIME`),
+  KEY `IDX_QRTZ_T_NFT_ST` (`SCHED_NAME`,`TRIGGER_STATE`,`NEXT_FIRE_TIME`),
+  KEY `IDX_QRTZ_T_NFT_MISFIRE` (`SCHED_NAME`,`MISFIRE_INSTR`,`NEXT_FIRE_TIME`),
+  KEY `IDX_QRTZ_T_NFT_ST_MISFIRE` (`SCHED_NAME`,`MISFIRE_INSTR`,`NEXT_FIRE_TIME`,`TRIGGER_STATE`),
+  KEY `IDX_QRTZ_T_NFT_ST_MISFIRE_GRP` (`SCHED_NAME`,`MISFIRE_INSTR`,`NEXT_FIRE_TIME`,`TRIGGER_GROUP`,`TRIGGER_STATE`),
+  CONSTRAINT `QRTZ_TRIGGERS_ibfk_1` FOREIGN KEY (`SCHED_NAME`, `JOB_NAME`, `JOB_GROUP`) REFERENCES `QRTZ_JOB_DETAILS` (`SCHED_NAME`, `JOB_NAME`, `JOB_GROUP`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `QRTZ_TRIGGERS` */
+
+insert  into `QRTZ_TRIGGERS`(`SCHED_NAME`,`TRIGGER_NAME`,`TRIGGER_GROUP`,`JOB_NAME`,`JOB_GROUP`,`DESCRIPTION`,`NEXT_FIRE_TIME`,`PREV_FIRE_TIME`,`PRIORITY`,`TRIGGER_STATE`,`TRIGGER_TYPE`,`START_TIME`,`END_TIME`,`CALENDAR_NAME`,`MISFIRE_INSTR`,`JOB_DATA`) values 
+('quartzScheduler','processMyJobTrigger','DEFAULT','processMyJob','DEFAULT',NULL,1531751280000,1531751220000,0,'WAITING','CRON',1531750782000,0,NULL,0,'');
 
 /*Table structure for table `roles` */
 
@@ -64,7 +295,7 @@ CREATE TABLE `user_profile` (
 /*Data for the table `user_profile` */
 
 insert  into `user_profile`(`User_Email_Id`,`Name`,`DOB`,`DOJ`,`MobileNo`,`Skype`,`LinkedIn`,`Facebook`,`Twitter`,`GooglePlus`,`Website`,`Address`,`ProfilePic`,`ApprovedBy`) values 
-('surajmba.mba@gmail.com','Suraj Sinha',NULL,'2017-08-26 13:24:54','','surajmba.mba@gmail.com','https://www.linkedin.com/in/suraj-sinha-909750b7/','http://www.facebook.com/suraj.sinha.77736','https://twitter.com/SurajSi73451444','https://plus.google.com/102616516840863727271','www.cybotech.in','Gola Road, Near Imambara,  Nawada, Bihar - 805110','profilePic/surajmba.mba@gmail.com.jpg','surajmba.mba@gmail.com');
+('faiz.krm@gmail.com','Suraj Sinha','2018-07-10','2017-08-26 13:24:54','','surajmba.mba@gmail.com','https://www.linkedin.com/in/suraj-sinha-909750b7/','http://www.facebook.com/suraj.sinha.77736','https://twitter.com/SurajSi73451444','https://plus.google.com/102616516840863727271','www.cybotech.in','Gola Road, Near Imambara,  Nawada, Bihar - 805110','profilePic/surajmba.mba@gmail.com.jpg','faiz.krm@gmail.com');
 
 /*Table structure for table `user_role` */
 
@@ -73,6 +304,7 @@ DROP TABLE IF EXISTS `user_role`;
 CREATE TABLE `user_role` (
   `User_Id` int(11) NOT NULL,
   `Role_Id` int(11) NOT NULL,
+  UNIQUE KEY `UK_cbsxin3pckjlc668m8hy4bvnd` (`Role_Id`),
   KEY `FK143BF46A7214966` (`Role_Id`),
   KEY `FK143BF46AAC4C0D46` (`User_Id`),
   CONSTRAINT `user_role_ibfk_1` FOREIGN KEY (`User_Id`) REFERENCES `users` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE,
@@ -82,9 +314,9 @@ CREATE TABLE `user_role` (
 /*Data for the table `user_role` */
 
 insert  into `user_role`(`User_Id`,`Role_Id`) values 
+(2,1),
 (2,2),
-(2,3),
-(2,1);
+(2,3);
 
 /*Table structure for table `user_token` */
 
@@ -100,12 +332,13 @@ CREATE TABLE `user_token` (
   PRIMARY KEY (`id`),
   KEY `FK_user_toke_user_idx` (`user_id`),
   CONSTRAINT `FK_user_role_user_idx` FOREIGN KEY (`user_id`) REFERENCES `users` (`Id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `user_token` */
 
 insert  into `user_token`(`id`,`user_id`,`last_used`,`secret_key`,`token`,`lastUsed`) values 
-(1,2,'2018-06-05 07:46:38','§⁄óç7bûÊE£ã\neî‡Bw¿ØËÄó¶W¬Úú∆CZíÚÓ~9M[\ZµRPîÖVeS|—3÷¥9gªx¯`ﬂ∏Y','eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIyIiwiQ0xBSU1fVE9LRU5fVkVSU0lPTiI6MC43MTM2ODEyNTIzMDE2NTA5fQ.RzrbkVNdEMCjYBKTJ1R8ehzBcGSTMAZdEPSi4GX34Rzei3hn6q83frKHjJUynnAc-NNbVNDvs30x_VwBb274mw','2018-06-05 13:16:36');
+(1,2,'2018-06-05 07:46:38','§⁄óç7bûÊE£ã\neî‡Bw¿ØËÄó¶W¬Úú∆CZíÚÓ~9M[\ZµRPîÖVeS|—3÷¥9gªx¯`ﬂ∏Y','eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIyIiwiQ0xBSU1fVE9LRU5fVkVSU0lPTiI6MC43MTM2ODEyNTIzMDE2NTA5fQ.RzrbkVNdEMCjYBKTJ1R8ehzBcGSTMAZdEPSi4GX34Rzei3hn6q83frKHjJUynnAc-NNbVNDvs30x_VwBb274mw','2018-06-05 13:16:36'),
+(4,2,'2018-07-12 10:06:02','yö¶(\rV9ä)th÷Æ˝∏L˚8@ÊUîzYFõãªsô;Ô>KEÉ{5T∑GÙÆè!¬∫˚Cû|≠≈S\"£S.Ú','eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIyIiwiQ0xBSU1fVE9LRU5fVkVSU0lPTiI6MC44MzI0NTkzOTk5MzYzODYyfQ.NtspypID4zUBVmpubVSVB_t2nH5rgH6qgkj5aK-05-jm5FTIuyk7bUkTIn8sK71KCjfaeqDL4yDajoiI_-N2aQ','2018-07-12 16:59:13');
 
 /*Table structure for table `users` */
 
@@ -126,7 +359,7 @@ CREATE TABLE `users` (
 /*Data for the table `users` */
 
 insert  into `users`(`Id`,`User_Email_Id`,`Password`,`Enable`) values 
-(2,'surajmba.mba@gmail.com','12345678',1);
+(2,'faiz.krm@gmail.com','12345678',1);
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
