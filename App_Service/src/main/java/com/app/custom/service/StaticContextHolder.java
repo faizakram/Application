2a8e5d4d9d4eb065c76584f9c68e190c51c18e1d
Faiz Akram <faiz.krm@gmail.com>
2018-07-18 19:16:41 +0530

    commit

---
+++ App_Service/src/main/java/com/app/custom/service/StaticContextHolder.java
@@ -0,0 +1,42 @@
+package com.app.custom.service;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StaticContextHolder implements BeanFactoryAware {
+
+    public static BeanFactory CONTEXT;
+
+    public StaticContextHolder() {
+    }
+
+    public static Object getBean(String s) throws BeansException {
+        return CONTEXT.getBean(s);
+    }
+
+    public static <T> T getBean(String s, Class<T> tClass) throws BeansException {
+        return CONTEXT.getBean(s, tClass);
+    }
+
+    public static <T> T getBean(Class<T> tClass) throws BeansException {
+        return CONTEXT.getBean(tClass);
+    }
+
+    public static Object getBean(String s, Object... objects) throws BeansException {
+        return CONTEXT.getBean(s, objects);
+    }
+
+    public static boolean containsBean(String s) {
+        return CONTEXT.containsBean(s);
+    }
+
+
+    @Override
+    public void setBeanFactory(BeanFactory applicationContext) throws BeansException {
+        CONTEXT = applicationContext;
+       
+    }
+}
