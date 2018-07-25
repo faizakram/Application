84d6f6a52971add9fcc70dafd55933df7785ffa4
Faiz Akram <faiz.krm@gmail.com>
2018-07-25 19:04:16 +0530

    commit

--- App_Dao/src/main/java/com/app/abs/AbstractDao.java
+++ App_Dao/src/main/java/com/app/abs/AbstractDao.java
@@ -2,28 +2,32 @@ package com.app.abs;
 
 import java.io.Serializable;
 import java.lang.reflect.ParameterizedType;
+import java.util.List;
 
 import org.hibernate.Criteria;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.PropertySource;
 
 import com.app.util.constant.CommonConstants;
+
 @PropertySource(CommonConstants.QUERY_PROPERTIES)
 public abstract class AbstractDao<PK extends Serializable, T> {
-	
+
 	private final Class<T> persistentClass;
-	
+
 	@SuppressWarnings("unchecked")
-	public AbstractDao(){
-		this.persistentClass =(Class<T>) ((ParameterizedType) this.getClass().getGenericSuperclass()).getActualTypeArguments()[1];
+	public AbstractDao() {
+		this.persistentClass = (Class<T>) ((ParameterizedType) this.getClass().getGenericSuperclass())
+				.getActualTypeArguments()[1];
 	}
-	
+
 	@Autowired
 	private SessionFactory sessionFactory;
 
-	protected Session getSession(){
+	protected Session getSession() {
 		return sessionFactory.getCurrentSession();
 	}
 
@@ -43,9 +47,30 @@ public abstract class AbstractDao<PK extends Serializable, T> {
 	public void delete(T entity) {
 		getSession().delete(entity);
 	}
-	
-	protected Criteria createEntityCriteria(){
+
+	protected Criteria createEntityCriteria() {
 		return getSession().createCriteria(persistentClass);
 	}
-
+	/*for Example */
+	@SuppressWarnings("unchecked")
+	public List<Object> findAllActiveBlog(Integer pageIndex, Integer pageSize, String name) {
+		try {
+			Query query = sessionFactory.getCurrentSession().createQuery("From Blog where name =:name order by blogDateTime desc");
+			
+			query.setString("name", name);
+			
+			query.setFirstResult((pageIndex - 1) * pageSize);
+			query.setMaxResults(pageSize);
+			
+			if (query.list() == null || query.list().isEmpty()) {
+				return null;
+			}
+			
+			return query.list();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 }
